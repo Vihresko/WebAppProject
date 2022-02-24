54773cd937ff12f58714469ee5d62447efd03224
Vihresko <vihrenstefanov@gmail.com>
2022-02-25 01:23:39 +0200

    start creating needed Db

--- WorkDiary/WorkDiary.Database/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WorkDiary.Database.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-EQU1II1\SQLEXPRESS;Database=WorkDiary;Trusted_Connection=true";
-    }
-}
