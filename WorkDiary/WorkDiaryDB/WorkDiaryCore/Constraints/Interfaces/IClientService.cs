945ca1e798916393990ea42cbb1745c149e9eae9
Vihresko <vihrenstefanov@gmail.com>
2022-03-02 15:33:28 +0200

    delete project core move services to webApp

--- WorkDiary/WorkDiaryDB/WorkDiaryCore/Constraints/Interfaces/IClientService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WorkDiaryCore.Models.Client;
-
-namespace WorkDiaryCore.Constraints.Interfaces
-{
-    public interface IClientService
-    {
-        public (bool isDone, string errors) AddNewClient(object addClientModel);
-    }
-}
