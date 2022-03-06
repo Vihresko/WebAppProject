1ad2579eddced031a608c9e41875f6d8bbbeabca
Vihresko <vihrenstefanov@gmail.com>
2022-03-06 17:56:57 +0200

    add textarea for description on procedures delete testFolder

---
+++ WorkDiary/WorkDiaryWebApp/Controllers/ProcedureController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkDiaryWebApp.Core.Constants;
+using WorkDiaryWebApp.Core.Interfaces;
+using WorkDiaryWebApp.Models.Procedure;
+
+namespace WorkDiaryWebApp.Controllers
+{
+    public class ProcedureController : Controller
+    {
+        private readonly IProcedureService procedureService;
+        public ProcedureController(IProcedureService _procedureService)
+        {
+            procedureService = _procedureService;
+        }
+        public IActionResult Procedures()
+        {
+            var model = procedureService.GetAllProcedures();
+            return View(model);
+        }
+
+        public IActionResult Procedure(string procedureId)
+        {
+            var model = procedureService.ProcedureInfo(procedureId);
+            return View(model);
+        }
+        public IActionResult AddProcedure()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProcedure(AddProcedureModel model)
+        {
+           
+            (bool isDone, string errors) = procedureService.AddNewProcedure(model);
+
+            if (isDone)
+            {
+                ViewData[MessageConstant.SuccessMessage] = "success";
+            }
+            else
+            {
+                ViewData[MessageConstant.ErrorMessage] = errors;
+            }
+            return View();
+        }
+
+        public IActionResult EditProcedure(string procedureId)
+        {
+            var model = procedureService.ProcedureInfo(procedureId);
+            return View(model);
+        }
+    }
+}
